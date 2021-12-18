d17d9227ec7deda0f29b5a4b41b9f31df490d007
JonathanVrijsen <84011890+JonathanVrijsen@users.noreply.github.com>
2021-12-18 09:30:06 +0100

    creation of the final project
    al wat klasses gemaakt etc

---
+++ Communication Networks Final Project/keyServer.py
@@ -0,0 +1,2 @@
+class keyServer:
+    pass
---
+++ Communication Networks Final Project/message.py
@@ -0,0 +1,11 @@
+from user import User
+
+#class message as saved in the server
+
+class Message:
+    sender = None
+    content = None #encrypted by symmetric key related to conversation
+
+    def __init__(self, sender, content):
+        self.sender=sender
+        self.content=content
