4ace6227b777320dc447de0157ae142fb8b788c9
Louis-Van-Eeckhoudt <62985294+Louis-Van-Eeckhoudt@users.noreply.github.com>
2021-12-18 16:41:17 +0100

    update 1641

--- Communication Networks Final Project/client.py
+++ Communication Networks Final Project/client.py
@@ -1,21 +1,37 @@
-from main import generateKeys
+from asymmetricKeying import generateKeys
 from user import User
+
 
 class Client:
     user = None
+    server_ip = None
+    server_socket = None
+    key_server_ip = None
+    key_server_socket = None
 
     def __init__(self):
-        (pubKey, privKey)=generateKeys()
+        (pubKey, privKey) = generateKeys()
+        (server_ip,server_socket,key_server_ip,key_server_socket) = self.get_server_information(self)
         pass
 
     def login(self, username, password):
-        #send username and password to keyserver
-        #if login successful, set current user of client
-        self.user=User(username, password)
+        # send username and password to keyserver
+        # if login successful, set current user of client
+        self.user = User(username, password)
+
+    def get_server_information(self):
+
+        return '127.0.0.1', 12000, '127.0.0.1', 12001
+
+    def get_conversations(self, server_ip, server_socket):
+        # request all the user's conversation from the server
+        pass
+    def send_message(self):
+        pass
 
     def logout(self):
-        #go back to begin screen
-        user=None
+        # go back to begin screen
+        user = None
 
-        #in a way, the keys of the previous user are still saved at the client.
-        #so maybe, implement a signal to let the keyServer know that some client has logged out, and hence it should create new keys for the conversations of the client
+        # in a way, the keys of the previous user are still saved at the client.
+        # so maybe, implement a signal to let the keyServer know that some client has logged out, and hence it should create new keys for the conversations of the client
