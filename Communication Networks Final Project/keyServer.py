3a726136f43e53acbf15eeb49c5fcda064ec8dd3
SamVandeVelde <58397500+SamVandeVelde@users.noreply.github.com>
2021-12-18 22:56:40 +0100

    temporary pseudocode for keyServer until I figure it out

--- Communication Networks Final Project/keyServer.py
+++ Communication Networks Final Project/keyServer.py
@@ -1,3 +1,5 @@
+import threading
+
 from user import User
 from socket import *
 import asymmetricKeying
@@ -10,6 +12,7 @@ class keyServer:
     serverSocket = None
     pubKey = None
     privKey = None
+    currentThreads = None
 
     def __init__(self):
         self.userArray = []
@@ -18,11 +21,20 @@ class keyServer:
         #SOCK_STREAM connection oriented -> two-way byte streams
         self.serverSocket.bind(('',self.serverPort)) #'' contains addresses, when empty it means all
         (self.pubKey, self.privKey) = asymmetricKeying.generateKeys()
+        self.currentThreads = []
 
     def listen(self):
         self.serverSocket.listen(64) #Number of allowed unnaccepted connections
-        connectionSocket, addr = self.serverSocket.accept() #return values: socket for client, and clientIP
+        connectionSocket, clientIP = self.serverSocket.accept() #return values: socket for client, and clientIP
 
-        newThread
+        newThread = threading.Thread(target=self.handle_message(), args=(connectionSocket, clientIP))
+        newThread.start()
 
+        self.currentThreads.append(newThread)
 
+    def handle_message(self,clientIP):
+        #step: if request for public key, send it, otherwise ignore
+        #step: decode message using private key and
+        #step: if register request, take account-password, check IP if sus?, check if accountname doesn't exist already
+        #if allright, create public and private key and send to receiver over temporary secure channel
+        #step: if login request, check combo and send to receiver over temporary secure channel
