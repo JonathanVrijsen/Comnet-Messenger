f3c2b4f4256c008a99734440e54b17162120b8f2
SamVandeVelde <58397500+SamVandeVelde@users.noreply.github.com>
2021-12-18 23:26:12 +0100

    outstring != content

--- Communication Networks Final Project/message.py
+++ Communication Networks Final Project/message.py
@@ -5,24 +5,26 @@ class MessageType(enum.Enum):
     publickeyrequest = 1
     registerrequest = 2
     loginrequest = 3
-    #add
+    #TODO add
 
 class Message:
     sender = None
     content = None #in main server: encrypted by symmetric key related to conversation
     messageType = None
+    outString = None
     def __init__(self, sender, messageType, content):
         self.sender=sender
-        #self.content=content
+        self.content=content
+        self.messageType = messageType;
 
         #enum switch-like attempt (Python lacks a proper enum switch
         if MessageType['messageType'] == 1:
-            self.content = "publickeyrequest - " + content #content = "clientIP"
+            self.outString = "publickeyrequest - " + content #content = "clientIP"
         elif MessageType['messageType'] == 2:
-            self.content = "registerrequest - " + content #content = "clientIP - username - password"
+            self.outString = "registerrequest - " + content #content = "clientIP - username - password"
             #todo encrypt
         elif MessageType['messageType'] == 3:
-            self.content = "loginrequest - " + content #content = "clientIP - username - password"
+            self.outString = "loginrequest - " + content #content = "clientIP - username - password"
             #todo encrypt
         #todo add if more cases
         #todo reject when else
