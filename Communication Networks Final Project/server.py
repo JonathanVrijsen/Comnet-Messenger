b1b50ef313b5ea86393bbc66c8c6480bfb415557
JonathanVrijsen <84011890+JonathanVrijsen@users.noreply.github.com>
2021-12-18 11:46:55 +0100

    update 1146

--- Communication Networks Final Project/server.py
+++ Communication Networks Final Project/server.py
@@ -1,5 +1,7 @@
 from user import User
 from socket import *
+import threading
+
 
 class connectedUser:
     def __int__(self, connectionSocket, user):
@@ -17,6 +19,17 @@ class Server:
 
     def listen(self):
         self.serverSocket.listen(64)
+        connectionSocket, addr = self.serverSocket.accept()
+        rcvdContent = connectionSocket.recv(1024)
+        #extract username from sender
+        username = "name"
+        #create user
+        newUser = User(username)
+        newConnectedUser = connectedUser(connectionSocket, newUser)
+        self.connectedUsers.append(newConnectedUser)
+
+    def listenToConnectedUser(self, connectedUser):
+        connectedUser.connectionSocket.listen(16)
 
 
 
