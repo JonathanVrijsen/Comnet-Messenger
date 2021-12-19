cfa42947cd17a33e124cc9439142e6d53b2c0c80
SamVandeVelde <58397500+SamVandeVelde@users.noreply.github.com>
2021-12-19 14:11:16 +0100

    Implemented custom error in bytesteam: suggested to implement in other py files

--- Communication Networks Final Project/byteStream.py
+++ Communication Networks Final Project/byteStream.py
@@ -1,6 +1,8 @@
 import byteStreamType
+import customError
 import byteStreamErrorTypes
 import re
+
 
 def constructor_info(message_type, content):
     # enum switch-like attempt (Python lacks a proper enum switch
@@ -11,16 +13,16 @@ def constructor_info(message_type, content):
     elif message_type == byteStreamType.ByteStreamType.loginrequest:
         out_string = "loginrequest - " + content  # content = "clientIP - username - password"
     else:
-        pass  # todo add if more cases
-        # todo reject ERROR when else
+        raise customError(byteStreamErrorTypes.ByteStreamErrorType.NoMessageTypeMatch)  # todo add if more cases
     out_stream = bytes(out_string, 'utf-8')
-    # todo ERROR HANDLING when failed?
     return message_type, content, out_stream
+
 
 def constructor_bytestream(out_stream):
     out_string = out_stream.decode("utf-8")
     content, message_type = extract_from_byte_string(out_string)
     return message_type, content, out_stream
+
 
 def extract_from_byte_string(out_string):
     if re.search(r"^publickeyrequest$", out_string) is not None:
@@ -33,15 +35,16 @@ def extract_from_byte_string(out_string):
         message_type = byteStreamType.ByteStreamType.loginrequest
         content = re.search(r"[\S]{5,20} - [\S]{8,20}$").group()
     else:
-        pass #todo error handling
+        raise customError(byteStreamErrorTypes.ByteStreamErrorType.NoMessageTypeMatch)
     return content, message_type
 
+
 class ByteStream:
-#    content = None  # in main server: encrypted by symmetric key related to conversation
-#    messageType = None
-#    outStream = None
+    #    content = None  # in main server: encrypted by symmetric key related to conversation
+    #    messageType = None
+    #    outStream = None
     def __init__(self, *args):
         if len(args) > 2:
             self.messageType, self.content, self.outStream = constructor_info(args[0], args[1], args[2])
         else:
-            self.messageType, self.content, self.outStream = constructor_bytestream(args[0],args[1])
+            self.messageType, self.content, self.outStream = constructor_bytestream(args[0], args[1])
