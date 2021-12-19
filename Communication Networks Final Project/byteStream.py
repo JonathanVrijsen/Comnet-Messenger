9f90c41b61a4367b236bd772fb5fa6478cb57666
SamVandeVelde <58397500+SamVandeVelde@users.noreply.github.com>
2021-12-19 12:00:21 +0100

    Bytestream more in sync with rest of project

--- Communication Networks Final Project/byteStream.py
+++ Communication Networks Final Project/byteStream.py
@@ -10,7 +10,7 @@ def extract_from_byte_string(out_string):
     elif re.search(r"^registerrequest - [\S]{5,20} - [\S]{8,20}$", out_string) is not None:
         message_type = byteStreamType.ByteStreamType.registerrequest
         content = re.search(r"[\S]{5,20} - [\S]{8,20}$").group()
-    elif re.search(r"^loginrequest - [\S]{5,20} - [\S]{8,20}$",out_string) is not None:
+    elif re.search(r"^loginrequest - [\S]{5,20} - [\S]{8,20}$", out_string) is not None:
         message_type = byteStreamType.ByteStreamType.loginrequest
         content = re.search(r"[\S]{5,20} - [\S]{8,20}$").group()
     else:
@@ -18,14 +18,12 @@ def extract_from_byte_string(out_string):
     return content, message_type
 
 class ByteStream:
-#    sender = None
 #    content = None  # in main server: encrypted by symmetric key related to conversation
 #    messageType = None
 #    outStream = None
 
 
     def __init__(self, sender_ip, message_type, content, public_key_sender):
-        self.senderIP = sender_ip
         self.content = content
         self.messageType = message_type
 
@@ -39,13 +37,10 @@ class ByteStream:
         else:
             pass#todo add if more cases
             #todo reject ERROR when else
-        self.outStream = asymmetricKeying.encrypt(content.encode('ascii'), out_string)
+        self.outStream = out_string.encode("ascii")
         #todo ERROR HANDLING when failed?
 
     def __init__(self, out_stream, private_key_receiver):
         self.outStream = out_stream
-        outstring = asymmetricKeying.decrypt(out_stream, private_key_receiver)
-        if outstring:
-            self.senderIP, self.content, self.messageType = extract_from_byte_string(outstring)
-        else:
-            pass #todo ERROR HANDLING
+        out_string = out_stream.decode("utf-8")
+        self.senderIP, self.content, self.messageType = extract_from_byte_string(outstring)
