29123991b5c20dcfc5f83754e27f207d1effac6e
SamVandeVelde <58397500+SamVandeVelde@users.noreply.github.com>
2021-12-19 01:40:38 +0100

    So this is how to easily en/decode a bytestream. I still need to add some more cases of streams sent (e.g. when sending a signature, that bytestream also needs to be prepared) but I'm done for tonight

--- Communication Networks Final Project/byteStream.py
+++ Communication Networks Final Project/byteStream.py
@@ -1,27 +1,54 @@
 import byteStreamType
+import asymmetricKeying
+import re
+
+
+def extract_from_string(out_string):
+    if re.search(r"^publickeyrequest - [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$", out_string) is not None:
+        message_type = byteStreamType.ByteStreamType.publickeyrequest
+        sender_ip = re.search(r"[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}", out_string).group()
+        content = None
+    elif re.search(r"^registerrequest - [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3} - [\S]{5,20} - [\S]{8,20}$",out_string) is not None:
+        message_type = byteStreamType.ByteStreamType.registerrequest
+        sender_ip = re.search(r"[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}", out_string).group()
+        content = re.search(r"[\S]{5,20} - [\S]{8,20}$").group()
+    elif re.search(r"^loginrequest - [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3} - [\S]{5,20} - [\S]{8,20}$",out_string) is not None:
+        message_type = byteStreamType.ByteStreamType.loginrequest
+        sender_ip = re.search(r"[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}", out_string).group()
+        content = re.search(r"[\S]{5,20} - [\S]{8,20}$").group()
+    else:
+        pass #todo error handling
+    return sender_ip, content, message_type
 
 class ByteStream:
-    sender = None
-    content = None  # in main server: encrypted by symmetric key related to conversation
-    messageType = None
-    outString = None
+#    sender = None
+#    content = None  # in main server: encrypted by symmetric key related to conversation
+#    messageType = None
+#    outStream = None
 
-    def __init__(self, sender, messageType, content):
-        self.sender=sender
-        self.content=content
-        self.messageType = messageType
+
+    def __init__(self, sender_ip, message_type, content, public_key_sender):
+        self.senderIP = sender_ip
+        self.content = content
+        self.messageType = message_type
 
         #enum switch-like attempt (Python lacks a proper enum switch
         if byteStreamType.ByteStreamType['messageType'] == 1:
-            self.outString = "publickeyrequest - " + content #content = "clientIP"
+            out_string = "publickeyrequest - " + content #content = "clientIP"
         elif byteStreamType.ByteStreamType['messageType'] == 2:
-            self.outString = "registerrequest - " + content #content = "clientIP - username - password"
-            #todo encrypt
+            out_string = "registerrequest - " + content #content = "clientIP - username - password"
         elif byteStreamType.ByteStreamType['messageType'] == 3:
-            self.outString = "loginrequest - " + content #content = "clientIP - username - password"
-            #todo encrypt
-        #todo add if more cases
-        #todo reject when else
+            out_string = "loginrequest - " + content #content = "clientIP - username - password"
+        else:
+            pass#todo add if more cases
+            #todo reject ERROR when else
+        self.outStream = asymmetricKeying.encrypt(content.encode('ascii'), out_string)
+        #todo ERROR HANDLING when failed?
 
-    def __init__(self, outString):
-        self.outString = outString
+    def __init__(self, out_stream, private_key_receiver):
+        self.outStream = out_stream
+        outstring = asymmetricKeying.decrypt(out_stream, private_key_receiver)
+        if outstring:
+            self.senderIP, self.content, self.messageType = extract_from_string(outstring)
+        else:
+            pass #todo ERROR HANDLING
