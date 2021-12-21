44bde2de8a4e6badce98afa269b2c9fffe294f8a
SamVandeVelde <58397500+SamVandeVelde@users.noreply.github.com>
2021-12-21 02:45:43 +0100

    following naming conventions: 2 of a lot

--- Communication Networks Final Project/asymmetric_keying.py
+++ Communication Networks Final Project/asymmetric_keying.py
@@ -29,30 +29,29 @@ def verify_sha1(content, signature, key):
         return False
 
 
-def rsa_sendable(msg, priv_key_sender, pub_key_receiver): #used by all the piers that need to send any content
-    #assuming message is a string
+def rsa_sendable(msg, priv_key_sender, pub_key_receiver):  # used by all the piers that need to send any content
+    # assuming message is a string
 
     cipher_msg = encrypt(msg, pub_key_receiver)
     signature = sign_sha1(msg, priv_key_sender)
     return signature + cipher_msg
 
 
-def rsa_receive(cipher_msg, pubKeySender, privKeyReceiver):
-    signature = cipher_msg[:128] #1024 bits key used, so signature is always 128 bytes
-    cipher_msg=cipher_msg[128:]
+def rsa_receive(cipher_msg, pub_key_sender, priv_key_receiver):
+    signature = cipher_msg[:128]  # 1024 bits key used, so signature is always 128 bytes
+    cipher_msg = cipher_msg[128:]
 
-    msg = decrypt(cipher_msg, privKeyReceiver)
+    msg = decrypt(cipher_msg, priv_key_receiver)
 
-    if verify_sha1(msg, signature, pubKeySender):
+    if verify_sha1(msg, signature, pub_key_sender):
         return msg
     else:
         return False
 
+
 def string_to_pubkey(pubkeystr):
     f, l = pubkeystr.split('(')
     n, e = l.split(',')
-    e = e[1:len(e)-1]
+    e = e[1:len(e) - 1]
     pub_key = rsa.PublicKey(int(n), int(e))
     return pub_key
-
-
--- Communication Networks Final Project/byte_stream.py
+++ Communication Networks Final Project/byte_stream.py
@@ -6,53 +6,53 @@ import re
 
 def constructor_info(message_type, content):
     # enum switch-like attempt (Python lacks a proper enum switch
-    if message_type == byteStreamType.ByteStreamType.publickeyrequest:
+    if message_type == byte_stream_type.ByteStreamType.publickeyrequest:
         out_string = "publickeyrequest - " + content  # content = "clientIP"
-    elif message_type == byteStreamType.ByteStreamType.registerrequest:
+    elif message_type == byte_stream_type.ByteStreamType.registerrequest:
         out_string = "registerrequest - " + content  # content = "clientIP - username - password"
-    elif message_type == byteStreamType.ByteStreamType.loginrequest:
+    elif message_type == byte_stream_type.ByteStreamType.loginrequest:
         out_string = "loginrequest - " + content  # content = "clientIP - username - password"
-    elif message_type == byteStreamType.ByteStreamType.registeranswer:
+    elif message_type == byte_stream_type.ByteStreamType.registeranswer:
         out_string = "registeranswer - " + content  # content = "succes/failed"
-    elif message_type == byteStreamType.ByteStreamType.passwordrequest:
+    elif message_type == byte_stream_type.ByteStreamType.passwordrequest:
         out_string = "passwordrequest - " + content  # content = "user exists"
-    elif message_type == byteStreamType.ByteStreamType.passwordanswer:
+    elif message_type == byte_stream_type.ByteStreamType.passwordanswer:
         out_string = "passwordanswer - " + content  # content = "password"
-    elif message_type == byteStreamType.ByteStreamType.loginanswer:
+    elif message_type == byte_stream_type.ByteStreamType.loginanswer:
         out_string = "loginanswer - " + content # content = "succes/failed"
-    elif message_type == byteStreamType.ByteStreamType.contactrequest:
+    elif message_type == byte_stream_type.ByteStreamType.contactrequest:
         out_string = "contactrequest" # content = \
-    elif message_type == byteStreamType.ByteStreamType.contactanswer:
+    elif message_type == byte_stream_type.ByteStreamType.contactanswer:
         out_string = "contactanswer - " + content # content = list of usernames
-    elif message_type == byteStreamType.ByteStreamType.keyrequest:
+    elif message_type == byte_stream_type.ByteStreamType.keyrequest:
         out_string = "keyrequest - " + str(content)  # content = public key of sender
-    elif message_type == byteStreamType.ByteStreamType.pubkeyanswer:
+    elif message_type == byte_stream_type.ByteStreamType.pubkeyanswer:
         out_string = "pubkeyanswer - " + str(content)  # content = public key of sender
-    elif message_type == byteStreamType.ByteStreamType.symkeyanswer:
+    elif message_type == byte_stream_type.ByteStreamType.symkeyanswer:
         out_string = "symkeyanswer - " + str(content)  # content = sym key of sender
-    elif message_type == byteStreamType.ByteStreamType.passwordcorrect:
+    elif message_type == byte_stream_type.ByteStreamType.passwordcorrect:
         out_string = "passwordcorrect - " + str(content)
-    elif message_type == byteStreamType.ByteStreamType.passwordwrong:
+    elif message_type == byte_stream_type.ByteStreamType.passwordwrong:
         out_string = "passwordwrong" # content = \
-    elif message_type == byteStreamType.ByteStreamType.registertomain:
+    elif message_type == byte_stream_type.ByteStreamType.registertomain:
         out_string = "registertomain - " + content # content = list of usernames
-    elif message_type == byteStreamType.ByteStreamType.newconversation:
+    elif message_type == byte_stream_type.ByteStreamType.newconversation:
         out_string = "newconversation - " + content #content is list of members
-    elif message_type == byteStreamType.ByteStreamType.message:
+    elif message_type == byte_stream_type.ByteStreamType.message:
         out_string = "message - " + content #content id (len(id) is 40) - message
-    elif message_type == byteStreamType.ByteStreamType.requestconversationkey:
+    elif message_type == byte_stream_type.ByteStreamType.requestconversationkey:
         out_string = "requestconversationkey - " + content
-    elif message_type == byteStreamType.ByteStreamType.requestmembers:
+    elif message_type == byte_stream_type.ByteStreamType.requestmembers:
         out_string = "requestmembers - " + content
-    elif message_type == byteStreamType.ByteStreamType.answermembers:
+    elif message_type == byte_stream_type.ByteStreamType.answermembers:
         out_string = "answermembers - " + content
-    elif message_type == byteStreamType.ByteStreamType.requestallids:
+    elif message_type == byte_stream_type.ByteStreamType.requestallids:
         out_string = "requestallids"
-    elif message_type == byteStreamType.ByteStreamType.answerallids:
+    elif message_type == byte_stream_type.ByteStreamType.answerallids:
         out_string = "answerallids - " + content
-    elif message_type == byteStreamType.ByteStreamType.getconversation:
+    elif message_type == byte_stream_type.ByteStreamType.getconversation:
         out_string = "getconversation - " + content
-    elif message_type == byteStreamType.ByteStreamType.conversation:
+    elif message_type == byte_stream_type.ByteStreamType.conversation:
         out_string = "conversation - " + content
     else:
         raise CustomError(ByteStreamErrorType.NoMessageTypeMatch)  # todo add if more cases
@@ -71,102 +71,102 @@ def constructor_bytestream(out_stream):
 
 def extract_from_byte_string(out_string):
     if re.search(r"^publickeyrequest$", out_string) is not None:
-        message_type = byteStreamType.ByteStreamType.publickeyrequest
+        message_type = byte_stream_type.ByteStreamType.publickeyrequest
         content = None
     elif re.search(r"^registerrequest - [\S]{1,99} - ", out_string) is not None:
-        message_type = byteStreamType.ByteStreamType.registerrequest
+        message_type = byte_stream_type.ByteStreamType.registerrequest
         (start, end) = re.search(r"registerrequest - ", out_string).span()
         content = out_string[end:]
     elif re.search(r"^loginrequest - ", out_string) is not None:
-        message_type = byteStreamType.ByteStreamType.loginrequest
+        message_type = byte_stream_type.ByteStreamType.loginrequest
         (start, end) = re.search(r"^loginrequest - ", out_string).span()
         print(start, end)
         content = out_string[end:]
     elif re.search(r"^registeranswer - ", out_string) is not None:
         (start, end) = re.search(r"^registeranswer - ", out_string).span()
-        message_type = byteStreamType.ByteStreamType.registeranswer
+        message_type = byte_stream_type.ByteStreamType.registeranswer
         content = out_string[end:]
     elif re.search(r"^loginanswer - ", out_string) is not None:
         (start, end) = re.search(r"^loginanswer - ", out_string).span()
-        message_type = byteStreamType.ByteStreamType.loginanswer
+        message_type = byte_stream_type.ByteStreamType.loginanswer
         content = out_string[end:]
     elif re.search(r"^passwordrequest - ", out_string) is not None:
         (start, end) = re.search(r"^passwordrequest - ", out_string).span()
-        message_type = byteStreamType.ByteStreamType.passwordrequest
+        message_type = byte_stream_type.ByteStreamType.passwordrequest
         content = out_string[end:]
     elif re.search(r"^passwordanswer - ", out_string) is not None:
         (start, end) = re.search(r"^passwordanswer - ", out_string).span()
-        message_type = byteStreamType.ByteStreamType.passwordanswer
+        message_type = byte_stream_type.ByteStreamType.passwordanswer
         content = out_string[end:]
     elif re.search(r"^contactrequest$", out_string) is not None:
-        message_type = byteStreamType.ByteStreamType.contactrequest
+        message_type = byte_stream_type.ByteStreamType.contactrequest
         content = ""
     elif re.search(r"^contactanswer - ", out_string) is not None:
-        message_type = byteStreamType.ByteStreamType.contactanswer
+        message_type = byte_stream_type.ByteStreamType.contactanswer
         (start, end) = re.search(r"^contactanswer - ", out_string).span()
         content = out_string[end:]
     elif re.search(r"^keyrequest - ", out_string) is not None:
         (start, end) = re.search(r"^keyrequest - ", out_string).span()
-        message_type = byteStreamType.ByteStreamType.keyrequest
+        message_type = byte_stream_type.ByteStreamType.keyrequest
         content = out_string[end:]
     elif re.search(r"^pubkeyanswer - ", out_string) is not None:
         (start, end) = re.search(r"^pubkeyanswer - ", out_string).span()
-        message_type = byteStreamType.ByteStreamType.pubkeyanswer
+        message_type = byte_stream_type.ByteStreamType.pubkeyanswer
         content = out_string[end:]
     elif re.search(r"^symkeyanswer - ", out_string) is not None:
         (start, end) = re.search(r"^symkeyanswer - ", out_string).span()
-        message_type = byteStreamType.ByteStreamType.symkeyanswer
+        message_type = byte_stream_type.ByteStreamType.symkeyanswer
         content = out_string[end:]
     elif re.search(r"^passwordcorrect - ", out_string) is not None:
         (start, end) = re.search(r"^passwordcorrect - ", out_string).span()
-        message_type = byteStreamType.ByteStreamType.passwordcorrect
+        message_type = byte_stream_type.ByteStreamType.passwordcorrect
         content = out_string[end:]
     elif re.search(r"^passwordwrong$", out_string) is not None:
-        message_type = byteStreamType.ByteStreamType.passwordwrong
+        message_type = byte_stream_type.ByteStreamType.passwordwrong
         content = ""
     elif re.search(r"^registertomain - ", out_string) is not None:
         (start, end) = re.search(r"^registertomain - ", out_string).span()
-        message_type = byteStreamType.ByteStreamType.registertomain
+        message_type = byte_stream_type.ByteStreamType.registertomain
         content = out_string[end:]
     elif re.search(r"^newconversation - ", out_string) is not None:
         (start, end) = re.search(r"^newconversation - ", out_string).span()
-        message_type = byteStreamType.ByteStreamType.newconversation
+        message_type = byte_stream_type.ByteStreamType.newconversation
         content = out_string[end:]
     elif re.search(r"^message - ", out_string) is not None:
         (start, end) = re.search(r"^message - ", out_string).span()
-        message_type = byteStreamType.ByteStreamType.message
+        message_type = byte_stream_type.ByteStreamType.message
         content = out_string[end:]
     elif re.search(r"^requestconversationkey - ", out_string) is not None:
         (start, end) = re.search(r"^requestconversationkey - ", out_string).span()
-        message_type = byteStreamType.ByteStreamType.requestconversationkey
+        message_type = byte_stream_type.ByteStreamType.requestconversationkey
         content = out_string[end:]
     elif re.search(r"^requestmembers - ", out_string) is not None:
         (start, end) = re.search(r"^requestmembers - ", out_string).span()
-        message_type = byteStreamType.ByteStreamType.requestmembers
+        message_type = byte_stream_type.ByteStreamType.requestmembers
         content = out_string[end:]
     elif re.search(r"^answermembers - ", out_string) is not None:
         (start, end) = re.search(r"^answermembers - ", out_string).span()
-        message_type = byteStreamType.ByteStreamType.answermembers
+        message_type = byte_stream_type.ByteStreamType.answermembers
         content = out_string[end:]
     elif re.search(r"^requestallids$", out_string) is not None:
         (start, end) = re.search(r"^requestallids$", out_string).span()
-        message_type = byteStreamType.ByteStreamType.requestallids
+        message_type = byte_stream_type.ByteStreamType.requestallids
         content = out_string[end:]
     elif re.search(r"^answerallids - ", out_string) is not None:
         (start, end) = re.search(r"^answerallids - ", out_string).span()
-        message_type = byteStreamType.ByteStreamType.answerallids
+        message_type = byte_stream_type.ByteStreamType.answerallids
         content = out_string[end:]
     elif re.search(r"^getconversation - ", out_string) is not None:
         (start, end) = re.search(r"^getconversation - ", out_string).span()
-        message_type = byteStreamType.ByteStreamType.getconversation
+        message_type = byte_stream_type.ByteStreamType.getconversation
         content = out_string[end:]
     elif re.search(r"^conversation - ", out_string) is not None:
         (start, end) = re.search(r"^conversation - ", out_string).span()
-        message_type = byteStreamType.ByteStreamType.conversation
+        message_type = byte_stream_type.ByteStreamType.conversation
         content = out_string[end:]
 
     else:
-        raise CustomError(byteStreamErrorTypes.ByteStreamErrorType.NoMessageTypeMatch)
+        raise CustomError(byte_stream_error_types.ByteStreamErrorType.NoMessageTypeMatch)
     return content, message_type
 
 
